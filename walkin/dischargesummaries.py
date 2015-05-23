9ac9dd065d50f56356cd5ff9eedca1de89d6ccd5
David Miller <david@deadpansincerity.com>
2015-05-23 19:33:34 +0100

    Add a letter for walkin nurses.
    refs openhealthcare/elcid#640

--- walkin/dischargesummaries.py
+++ walkin/dischargesummaries.py
@@ -3,7 +3,13 @@ Discharge summary template for Walkin
 """
 from dischargesummary import DischargeTemplate
 
-class WalkinDischargeLetter(DischargeTemplate):
-    name = 'walkin'
+class WalkinDoctorDischargeLetter(DischargeTemplate):
+    name = 'walkindoctor'
     template = 'walkin_discharge_letter.html'
     button_display = 'Clinic Letter'
+
+
+class WalkinNurseDischargeLetter(DischargeTemplate):
+    name = 'walkinnurse'
+    template = 'walkin_nurse_discharge_letter.html'
+    button_display = 'Nurse Clinic Letter'
