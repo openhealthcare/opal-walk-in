17af588be581661b44ceb72c67bd51dd344ee136
David Miller <david@deadpansincerity.com>
2015-09-17 16:26:20 +0100

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='opal-walk-in',
-    version='0.1',
+    version='0.2',
     packages=['walkin'],
     include_package_data=True,
     license='GPL3',
