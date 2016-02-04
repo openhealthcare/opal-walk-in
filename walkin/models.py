b31acb493510dfc1c0fce856371ba3db624134d9
David Miller <david@deadpansincerity.com>
2016-02-04 16:55:45 +0000

    Saves Zika data

--- walkin/models.py
+++ walkin/models.py
@@ -98,3 +98,14 @@ class WalkinNurseLedCare(omodels.EpisodeSubrecord):
 
     reason    = ForeignKeyOrFreeText(Wi_nurse_reason)
     treatment = models.TextField(blank=True, null=True)
+
+class ZikaPathway(omodels.EpisodeSubrecord):
+    _icon = 'fa fa-warning'
+
+    pregnant           = models.BooleanField(default=False)
+    gestation          = models.CharField(max_length=255, blank=True, null=True)
+    due_date           = models.CharField(max_length=255, blank=True, null=True)
+    antenatal_hospital = models.CharField(max_length=255, blank=True, null=True)
+    yellow_fever       = models.CharField(max_length=255, blank=True, null=True)
+    advice             = models.TextField(blank=True, null=True)
+    follow_up          = models.TextField(blank=True, null=True)
